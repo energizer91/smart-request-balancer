3a6c440a568b9a79826707fa7dada90cffd980aa
Alexander Bareyko <militaryboy@yandex.ru>
2024-05-16 14:45:48 +0200

    move tests to jest instead

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov'],
+  testMatch: ['<rootDir>/test/**/*.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,5 @@
-/* tslint:disable:no-unused-expression */
-import { expect, use } from 'chai';
-import { describe, it } from 'mocha';
-import sinon from 'sinon';
-import sinonChai from 'sinon-chai';
+import { describe, expect, it } from '@jest/globals';
 import SmartQueue from '../src';
-
-use(sinonChai);
 
 const ERROR_RATE = 10;
 const params = {
@@ -33,90 +27,87 @@ describe('Smart queue', () => {
   it('should be defined', () => {
     const queue = new SmartQueue(params);
 
-    expect(queue).not.to.be.undefined;
+    expect(queue).toBeDefined();
   });
 
   it('should be an object', () => {
     const queue = new SmartQueue(params);
 
-    expect(queue).to.be.an('object');
+    expect(queue).toBeInstanceOf(Object);
   });
 
   it('should have all required methods and fields', () => {
     const queue = new SmartQueue(params);
 
-    expect(queue).to.have.property('request');
-    expect(queue).to.have.property('totalLength');
-    expect(queue).to.have.property('isOverheated');
+    expect(queue).toHaveProperty('request');
+    expect(queue).toHaveProperty('totalLength');
+    expect(queue).toHaveProperty('isOverheated');
   });
 
   it('should make requests', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub().returns(1);
+    const request = jest.fn(async () => 1);
 
     const result = await queue.request(request);
 
-    expect(result).to.eq(1);
+    expect(result).toEqual(1);
   });
 
   it('should cool down queue after request', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub();
+    const request = jest.fn();
 
     await queue.request(request);
 
-    expect(queue.totalLength).to.eq(0);
-    expect(queue.isOverheated).to.eq(false);
+    expect(queue.totalLength).toBe(0);
+    expect(queue.isOverheated).toBe(false);
   });
 
   it('should measure length', () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub();
+    const request = jest.fn();
 
     queue.request(request);
     queue.request(request);
     queue.request(request);
 
-    expect(queue.totalLength).to.eq(3);
+    expect(queue.totalLength).toBe(3);
   });
 
   it('should have length 0 after request', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub();
+    const request = jest.fn();
 
     await queue.request(request);
     await queue.request(request);
     await queue.request(request);
 
-    expect(queue.totalLength).to.eq(0);
+    expect(queue.totalLength).toBe(0);
   });
 
   it('should execute sequentally', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon
-      .stub()
-      .onFirstCall()
-      .returns(1)
-      .onSecondCall()
-      .returns(2)
-      .onThirdCall()
-      .returns(3);
-    const callback = sinon.spy();
+    const request = jest
+      .fn()
+      .mockResolvedValueOnce(1)
+      .mockResolvedValueOnce(2)
+      .mockResolvedValueOnce(3);
+    const callback = jest.fn();
 
     await queue.request(request).then(callback);
     await queue.request(request).then(callback);
     await queue.request(request).then(callback);
 
-    expect(callback).to.have.been.calledThrice;
-    expect(callback.getCall(0)).to.have.been.calledWith(1);
-    expect(callback.getCall(1)).to.have.been.calledWith(2);
-    expect(callback.getCall(2)).to.have.been.calledWith(3);
+    expect(callback).toHaveBeenCalledTimes(3);
+    expect(callback.mock.calls[0][0]).toBe(1);
+    expect(callback.mock.calls[1][0]).toBe(2);
+    expect(callback.mock.calls[2][0]).toBe(3);
   });
 
   it('should not execute calls faster than rate limit', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub();
-    const callback = sinon.spy();
+    const request = jest.fn();
+    const callback = jest.fn();
     const rateLimit = Math.round((params.rules.common.limit / params.rules.common.rate) * 1000);
 
     await queue.request(request).then(callback);
@@ -124,12 +115,12 @@ describe('Smart queue', () => {
     await queue.request(request).then(callback);
     const secondEnd = Date.now();
 
-    expect(Math.abs(secondEnd - firstEnd - rateLimit)).is.lte(ERROR_RATE);
+    expect(Math.abs(secondEnd - firstEnd - rateLimit)).toBeLessThanOrEqual(ERROR_RATE);
   });
 
   it('should make retry', async () => {
     const queue = new SmartQueue(params);
-    const callback = sinon.spy();
+    const callback = jest.fn();
     let retryFlag = false;
 
     await queue
@@ -145,13 +136,13 @@ describe('Smart queue', () => {
       })
       .then(callback);
 
-    expect(callback).to.have.been.calledOnce;
-    expect(callback).to.have.been.calledWith(1);
+    expect(callback).toBeCalledTimes(1);
+    expect(callback).toBeCalledWith(1);
   });
 
   it('should make retry with default config param', async () => {
     const queue = new SmartQueue({ ...params, retryTime: 0.1 });
-    const callback = sinon.spy();
+    const callback = jest.fn();
     let retryFlag = false;
 
     await queue
@@ -167,19 +158,21 @@ describe('Smart queue', () => {
       })
       .then(callback);
 
-    expect(callback).to.have.been.calledOnce;
-    expect(callback).to.have.been.calledWith(1);
+    expect(callback).toBeCalledTimes(1);
+    expect(callback).toBeCalledWith(1);
   });
 
   it('should return error', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub().throws();
-    const callback = sinon.spy();
+    const request = jest.fn().mockImplementationOnce(() => {
+      throw new Error('error');
+    });
+    const callback = jest.fn();
 
     await queue.request(request).catch(callback);
 
-    expect(callback).to.have.been.calledOnce;
-    expect(callback).to.have.been.calledWith(sinon.match.instanceOf(Error));
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith(expect.any(Error));
   });
 
   it('should hit overall heat limit', async () => {
@@ -194,41 +187,38 @@ describe('Smart queue', () => {
       overall: overallRule
     });
     const rateLimit = Math.round((overallRule.limit / overallRule.rate) * 1000);
-    const request = sinon.stub();
-    const callback = sinon.spy();
+    const request = jest.fn();
+    const callback = jest.fn();
 
     await queue.request(request).then(callback);
     const firstEnd = Date.now();
     await queue.request(request).then(callback);
     const secondEnd = Date.now();
 
-    expect(secondEnd - firstEnd).is.gte(rateLimit);
+    expect(secondEnd - firstEnd).toBeGreaterThanOrEqual(rateLimit);
   });
 
   it('should create new rule if nothing found', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon.stub().returns(1);
-    const callback = sinon.spy();
+    const request = jest.fn().mockResolvedValue(1);
+    const callback = jest.fn();
 
     await queue.request(request, '1', 'lol').then(callback);
 
-    expect(callback).to.have.been.calledOnce;
-    expect(callback).to.have.been.calledWith(1);
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(callback).toHaveBeenCalledWith(1);
     // @ts-ignore
-    expect(queue.params.rules).to.have.property('lol');
+    expect(queue.params.rules).toHaveProperty('lol');
   });
 
   it('should prioritize calls', async () => {
     const queue = new SmartQueue(params);
-    const request = sinon
-      .stub()
-      .onFirstCall()
-      .returns(1)
-      .onSecondCall()
-      .returns(2)
-      .onThirdCall()
-      .returns(3);
-    const callback = sinon.spy();
+    const request = jest
+      .fn()
+      .mockResolvedValueOnce(1)
+      .mockResolvedValueOnce(2)
+      .mockResolvedValueOnce(3);
+    const callback = jest.fn();
 
     await Promise.all([
       queue.request(request, '1', 'group').then(callback),
@@ -236,10 +226,10 @@ describe('Smart queue', () => {
       queue.request(request, '3', 'individual').then(callback)
     ]);
 
-    expect(callback).to.have.been.calledThrice;
-    expect(callback).to.have.been.calledWith(1);
-    expect(callback).to.have.been.calledWith(3);
-    expect(callback).to.have.been.calledWith(2);
+    expect(callback).toHaveBeenCalledTimes(3);
+    expect(callback).toHaveBeenCalledWith(1);
+    expect(callback).toHaveBeenCalledWith(3);
+    expect(callback).toHaveBeenCalledWith(2);
   });
 
   it('should not wait more than rate limit time', async () => {
@@ -253,7 +243,7 @@ describe('Smart queue', () => {
       queue.request(request).then(() => (secondEnd = Date.now()))
     ]);
 
-    expect(Math.abs(secondEnd - firstEnd - 33)).is.lte(ERROR_RATE);
+    expect(Math.abs(secondEnd - firstEnd - 33)).toBeLessThanOrEqual(ERROR_RATE);
   });
 
   it('should execute tasks regardless of execution time', async () => {
@@ -275,7 +265,7 @@ describe('Smart queue', () => {
       queue.request(request).then(() => (secondEnd = Date.now()))
     ]);
 
-    expect(Math.abs(secondEnd - firstEnd - 200)).is.lte(ERROR_RATE);
+    expect(Math.abs(secondEnd - firstEnd - 200)).toBeLessThanOrEqual(ERROR_RATE);
   });
 
   it('should properly schedule requests on multiple queues', async () => {
@@ -319,9 +309,9 @@ describe('Smart queue', () => {
       queue.request(request3, 'q2', 'q2'),
       queue.request(request4, 'q2', 'q2')
     ]);
-    expect(Math.abs(r3Start - r1Start)).is.lte(ERROR_RATE);
-    expect(Math.abs(r4Start - r1Start - 100)).is.lte(ERROR_RATE);
-    expect(Math.abs(r2Start - r1Start - 250)).is.lte(ERROR_RATE);
+    expect(Math.abs(r3Start - r1Start)).toBeLessThanOrEqual(ERROR_RATE);
+    expect(Math.abs(r4Start - r1Start - 100)).toBeLessThanOrEqual(ERROR_RATE);
+    expect(Math.abs(r2Start - r1Start - 250)).toBeLessThanOrEqual(ERROR_RATE);
   });
 
   it('should clear queues', () => {
@@ -338,10 +328,10 @@ describe('Smart queue', () => {
     queue.request(() => Promise.resolve(true), '1', 'q1');
     queue.request(() => Promise.resolve(true), '2', 'q1');
 
-    expect(queue.totalLength).to.eql(2);
+    expect(queue.totalLength).toBe(2);
 
     queue.clear();
 
-    expect(queue.totalLength).to.eql(0);
+    expect(queue.totalLength).toBe(0);
   });
 });
